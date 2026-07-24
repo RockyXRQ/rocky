bea0219806ca6f0899a7c4ad51ff8c90b160e31b
Rocky_ <rockyxrq@gmail.com>
2026-07-24 14:15:59 +0800

    add FRC 2026 undefeated record

--- index.py
+++ index.py
@@ -146,6 +146,7 @@ galleries = [
                 ],
                 links=[
                     award("FRC 2026 China Off-season Champion"),
+                    award("FRC 2026 China Off-season Undefeated (24-0-0)"),
                     award("FRC 2026 China Off-season Engineering Inspiration Award"),
                     award("FRC 2026 Shanghai Regional Champion"),
                     award("FRC 2026 Houston Championship Innovation in Control Award"),
